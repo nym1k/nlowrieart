72f8e7080605a3bbe4d944c052faa5cafff8c89f
Ross King <sleeping.stars@gmail.com>
2017-02-22 10:17:10 +0000

    Rearranged app.js to fix infinite loading issue, changed cloudflare url in index.html to https

--- js/app.js
+++ js/app.js
@@ -1,87 +1,82 @@
-$(function() {
+// instagrab
+var token = '52931610.272b9cb.cb4f5faaff6e4a57bd9a87e2eb9ea11e',
+    maxPhotos = 12;
 
-  // instagrab
-  var token = '52931610.272b9cb.cb4f5faaff6e4a57bd9a87e2eb9ea11e',
-      maxPhotos = 12;
+$.ajax({
+  url: 'https://api.instagram.com/v1/users/self/media/recent?count=' + maxPhotos + '&access_token=' + token,
+  dataType: 'jsonp',
+	type: 'GET',
+	success: function(data){
+		for( x in data.data ){
+      var caption = data.data[x].caption.text.substring(0, 97) + '...';
+			$('.feed').append('<a href="'+data.data[x].link+'" alt="instagram photo" class="photobox"><div class="photo" style="background: url('+data.data[x].images.low_resolution.url+'); background-position: center; background-size: cover;"></div><p class="caption">' + caption + '</p></div>');
+		}
+    window.sr = ScrollReveal({reset: true});
+    sr.reveal('.photobox', { duration: 800 });
+	},
+	error: function(data){
+		console.log(data);
+	}
+});
 
-  $.ajax({
-    url: 'https://api.instagram.com/v1/users/self/media/recent?count=' + maxPhotos + '&access_token=' + token,
-    dataType: 'jsonp',
-  	type: 'GET',
-  	success: function(data){
-   		console.log(data);
-  		for( x in data.data ){
-        var caption = data.data[x].caption.text.substring(0, 97) + '...';
-  			$('.feed').append('<a href="'+data.data[x].link+'" alt="instagram photo" class="photobox"><div class="photo" style="background: url('+data.data[x].images.low_resolution.url+'); background-position: center; background-size: cover;"></div><p class="caption">' + caption + '</p></div>');
-  		}
-      window.sr = ScrollReveal({reset: true});
-      sr.reveal('.photobox', { duration: 800 });
-  	},
-  	error: function(data){
-  		console.log(data);
-  	}
-  });
-
-  // Loading Screen
-  $(window).on('load', function() {
-    $(".se-pre-con").fadeOut("slow");;
-  });
-
-  // Full Screen Hero
-  checkSize();
-  $(window).resize(checkSize);
-  function checkSize() {
-    var height = $('#hero').height();
-    $('.fullscreen').css('height', height);
-  }
-
-  // Apply Header Style After Hero
-  $(window).scroll(function() {
-  var scroll = $(window).scrollTop();
+// Full Screen Hero
+checkSize();
+$(window).resize(checkSize);
+function checkSize() {
   var height = $('#hero').height();
-  var width = $(window).width();
+  $('.fullscreen').css('height', height);
+}
 
-  if (scroll >= height && width > 990) {
-    $(".navbar").addClass("darkHeader");
-    $(".navbar-brand").css('display', 'inline-block');
-  } else {
-    $(".navbar").removeClass("darkHeader");
-    $(".navbar-brand").css('display', 'none');
-  }
-  });
+// Apply Header Style After Hero
+$(window).scroll(function() {
+var scroll = $(window).scrollTop();
+var height = $('#hero').height();
+var width = $(window).width();
 
-  // Scrollspy
-  $('body').scrollspy({ target: '.navbar' })
+if (scroll >= height && width > 990) {
+  $(".navbar").addClass("darkHeader");
+  $(".navbar-brand").css('display', 'inline-block');
+} else {
+  $(".navbar").removeClass("darkHeader");
+  $(".navbar-brand").css('display', 'none');
+}
+});
 
-  // Smooth Scroll
-  $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        if (target.length) {
-          $('html, body').animate({
-            scrollTop: target.offset().top
-          }, 1000);
-          return false;
-        }
+// Scrollspy
+$('body').scrollspy({ target: '.navbar' })
+
+// Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
       }
-    });
-  });
-
-  // Collapse Mobile Menu on button click
-  $('.navbar-collapse a').click(function (e) {
-    if (window.innerWidth < 992) {
-      $('.navbar-collapse').collapse('toggle');
     }
   });
+});
 
-  // Click outside of menu to close
-  $('html').click(function() {
-    var menu = $('.navbar-collapse')
-    if (menu.hasClass( "show")) {
-      menu.collapse('toggle');
-    }
-  });
+// Collapse Mobile Menu on button click
+$('.navbar-collapse a').click(function (e) {
+  if (window.innerWidth < 992) {
+    $('.navbar-collapse').collapse('toggle');
+  }
+});
 
+// Click outside of menu to close
+$('html').click(function() {
+  var menu = $('.navbar-collapse')
+  if (menu.hasClass( "show")) {
+    menu.collapse('toggle');
+  }
+});
+
+// Loading Screen
+$(window).on('load', function() {
+  $(".se-pre-con").fadeOut("slow");;
 });
